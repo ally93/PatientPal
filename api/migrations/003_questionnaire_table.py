7a2ac38fcd8af448e3982e2f1d328eacb6393def
Sophie <sophie.nguyen94@yahoo.com>
2022-11-18 18:46:32 -0500

    finished table

---
+++ api/migrations/003_questionnaire_table.py
@@ -0,0 +1,23 @@
+steps= [
+    [
+        ## create a table
+        """
+        CREATE TABLE questionnaires (
+            id SERIAL PRIMARY KEY NOT NULL,
+            medications VARCHAR(10000),
+            surgeries VARCHAR(10000),
+            concerns VARCHAR(10000),
+            weight SMALLINT,
+            blood_pressure VARCHAR(50),
+            date DATE,
+            patient_id INTEGER NOT NULL REFERENCES patients("id")
+
+        );
+        """,
+
+        ##drop the table
+        """
+        DROP TABLE questionnaires;
+        """
+    ]
+]
