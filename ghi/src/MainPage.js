622ab4a6126cabf99c1a27a04381ac80e39ea456
Ally <allygoose93@gmail.com>
2022-11-28 16:39:32 -0800

    rough photo

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,9 +1,12 @@
-
+import familyphoto from './images/familyphoto.jpeg'
 function MainPage() {
   return (
-
-      <h1>testing</h1>
-
+    <>
+      <h1>My Patient Portal</h1>
+      <div>
+        <img src={familyphoto} alt="fam" />
+      </div>
+    </>
   );
 }
 
