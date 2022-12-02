45c8f41bf6258befe53643f2118afe0e8ea32180
Sophie <sophie.nguyen94@yahoo.com>
2022-12-02 18:22:29 -0500

    unit test to create questionnaire

--- api/tests/test_questionnaires.py
+++ api/tests/test_questionnaires.py
@@ -22,3 +22,47 @@ def test_get_all_questionnaires():
 
     #clean up
     app.dependency_overrides ={}
+
+
+class CreateQuestionnaireQueries:
+    def create(self, questionnaire):
+        result = {
+            "id": 1
+        }
+        result.update(questionnaire)
+        return result
+
+def test_create_questionnaire():
+    # Arrange
+    app.dependency_overrides[QuestionnaireRepository] = CreateQuestionnaireQueries
+
+    json = {
+            "medications":"banana",
+            "surgeries":"banana",
+            "concerns":"banana",
+            "weight":999,
+            "blood_pressure":"banana",
+            "date": "2022-11-11",
+            }
+
+
+    expected = {
+        "id":1,
+        "medications":"banana",
+        "surgeries":"banana",
+        "concerns":"banana",
+        "weight":999,
+        "blood_pressure":"banana",
+        "date": "2022-11-11",
+        }
+
+
+    # Act
+    response = client.post("/api/patient/0/questionnaire/create" ,json=json)
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == expected
+
+    # Clean up
+    app.dependency_overrides = {}
