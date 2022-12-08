a7482e2792d694713c2a71aa632d8df45f07a088
Tanner Halsey <tannerhalsey@me.com>
2022-12-07 17:50:00 -0700

    loggingout..oflife

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -14,50 +14,69 @@ import QuestionnaireEdit from "./QuestionnaireEdit";
 // import MainPage from "./MainPage";
 // import Nav from "./Nav";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import { useAuthContext } from "./useToken";
 import LoginForm from "./Login";
-
+import LogOut from "./Logout";
+import SignOut from "./Logout";
 
 
 function App() {
   const { token } = useAuthContext();
 
-if (token){
+  if (token) {
     return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="patients" element={<PatientsList />} />
+
+            <Route path="dashboard/logout" element={<SignOut />} />
+            <Route path="logout" element={<SignOut />} />
+
+            <Route path="patient/:patient_id" element={<PatientDetail />} />
+            <Route
+              path="patient/:patient_id/update/"
+              element={<PatientUpdate />}
+            />
+            <Route path="patient/create" element={<PatientForm />} />
+            <Route path="questionnaires" element={<QuestionnairesList />} />
+
+            <Route path="dashboard" element={<MainPage />} />
+            <Route
+              path="patient/:patient_id/questionnaires"
+              element={<QuestionnairesList />}
+            />
+            <Route
+              path="questionnaire/:questionnaire_id"
+              element={<QuestionnaireDetail />}
+            />
+            <Route
+              path="patient/:patient_id/questionnaire/create"
+              element={<QuestionnaireForm />}
+            />
+            <Route
+              path="patient/:patient_id/questionnaire/:questionnaire_id/edit"
+              element={<QuestionnaireEdit />}
+            />
+          </Routes>
+
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  } else {
+    return (
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="patients" element={<PatientsList />} />
-          <Route path="patient/:patient_id" element={<PatientDetail />} />
-          <Route path="patient/:patient_id/update/" element={<PatientUpdate />} />
-          <Route path="patient/create" element={<PatientForm />} />
-          <Route path="questionnaires" element={<QuestionnairesList />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="dashboard" element={<MainPage />} />
-          <Route path="patient/:patient_id/questionnaires" element={<QuestionnairesList />} />
-          <Route path="questionnaire/:questionnaire_id" element={<QuestionnaireDetail />} />
-          <Route path="patient/:patient_id/questionnaire/create" element={<QuestionnaireForm />} />
-          <Route path="patient/:patient_id/questionnaire/:questionnaire_id/edit" element={<QuestionnaireEdit />} />
+          <Route path="/" element={<LoginForm />} />
+          <Route path="register" element={<ResgisterForm />} />
         </Routes>
-
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-} else {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="register" element={<ResgisterForm />} />
-      </Routes>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  }
 }
-}
-
 
 export default App;
---
+++ ghi/src/Logout.js
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "./useToken";
+
+let internalToken = null;
+function SignOut() {
+  const navigate = useNavigate();
+  const { token } = useAuthContext();
+  async function logout() {
+    if (token) {
+      const url = `${process.env.REACT_APP_ACCOUNTS_API_HOST}/token`;
+      await fetch(url, {
+        method: "delete",
+        credentials: "include",
+      });
+      navigate("/logout");
+    } else {
+      console.log("Not validtoken");
+      navigate("/");
+    }
+  }
+  const submitHandler = (e) => {
+    logout();
+    e.preventDefault();
+  };
+  return (
+    <div className="logout-container">
+      <center>
+        <form onSubmit={submitHandler}>
+          <p className="Verification">Verify Logout</p>
+          <button className="logout-btn">Logout</button>
+        </form>
+      </center>
+    </div>
+  );
+}
+export default SignOut;
